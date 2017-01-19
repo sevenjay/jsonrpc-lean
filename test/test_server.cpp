5b3c5b74e65563322aed310798c1e01a7d1898de
Juliano Siloto Assine <jsiloto@cpqd.com.br>
2017-01-19 17:37:34 -0200

    add invoke tests and coverage check

--- test/test_server.cpp
+++ test/test_server.cpp
@@ -5,9 +5,6 @@
  *      Author: jsiloto
  */
 
-
-
-
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <array>
@@ -20,9 +17,9 @@ using testing::_;
 using testing::Args;
 using testing::ElementsAreArray;
 using testing::Invoke;
+using testing::Return;
 
-
-class Math {
+class Methods {
 public:
     double Add(double a, double b) {
         return a + b;
@@ -32,78 +29,123 @@ public:
         return std::accumulate(a.begin(), a.end(), double(0),
             [](const double& a, const Json& b) { return a + b.number_value(); });
     };
+
+    std::string Concat(const std::string& a, const std::string& b) {
+        return a + b;
+    }
+
+    std::string FromBinary(const Json& b) {
+        return{ b.string_value().begin(), b.string_value().end() };
+    }
+
+    Json::object ToObject(const Json::array& a) {
+        Json::object s;
+        for (size_t i = 0; i < a.size(); ++i) {
+            s[std::to_string(i)] = Json(a[i]);
+        }
+        return s;
+    }
 };
 
-std::string Concat(const std::string& a, const std::string& b) {
-    return a + b;
-}
+class MethodsMock : public Methods {
+ public:
+  MOCK_METHOD2(Add, double(double a, double b));
+  MOCK_METHOD1(AddArray, double(const Json::array& a));
+  MOCK_METHOD2(Concat, std::string(const std::string& a, const std::string& b));
+  MOCK_METHOD1(ToObject, Json::object(const Json::array& a));
+};
 
-std::string FromBinary(const Json& b) {
-    return{ b.string_value().begin(), b.string_value().end() };
-}
-
-Json::object ToStruct(const Json::array& a) {
-    Json::object s;
-    for (size_t i = 0; i < a.size(); ++i) {
-        s[std::to_string(i)] = Json(a[i]);
-    }
-    return s;
-}
 
 void PrintNotification(const std::string& a) {
     printf("notification %s\n", a.c_str());
 }
 
-class JsonRpcTest : public ::testing::Test {
- protected:
+MethodsMock GlobalMock;
+
+std::string StaticConcat(const std::string& a, const std::string& b){
+    return GlobalMock.Concat(a, b);
+}
+Json::object StaticToObject (const Json::array& a){
+    return GlobalMock.ToObject(a);
+}
+
+class JsonRpcTest: public ::testing::Test {
+
+public:
+
+protected:
+    void CheckExpectedResponse(std::string r){
+        EXPECT_EQ(r, expectedResponse);
+        printf("response: %s\n\n", r.c_str());
+    }
+
     jsonrpc::Server server;
+    Methods methods;
+
+    std::string response;
+    std::string expectedResponse;
 
     std::string addRequest = "{\"jsonrpc\":\"2.0\",\"method\":\"add\",\"id\":0,\"params\":[3,2]}";
     std::string concatRequest = "{\"jsonrpc\":\"2.0\",\"method\":\"concat\",\"id\":1,\"params\":[\"Hello, \",\"World!\"]}";
-    std::string addArrayRequest = "{\"jsonrpc\":\"2.0\",\"method\":\"add_array\",\"id\":2,\"params\":[[1000,2147483647]]}";
-    std::string toStructRequest = "{\"jsonrpc\":\"2.0\",\"method\":\"to_struct\",\"id\":4,\"params\":[[12,\"foobar\",[12,\"foobar\"]]]}";
+    std::string addArrayRequest = "{\"jsonrpc\":\"2.0\",\"method\":\"add_array\",\"id\":2,\"params\":[[1111,2222]]}";
+    std::string toObjectRequest = "{\"jsonrpc\":\"2.0\",\"method\":\"to_object\",\"id\":4,\"params\":[[12,\"foobar\",[12,\"foobar\"]]]}";
     std::string printNotificationRequest = "{\"jsonrpc\":\"2.0\",\"method\":\"print_notification\",\"params\":[\"This is just a notification, no response expected!\"]}";
 
 };
 
-/// @test Test AD7490::RunRuntimeError.
-TEST_F(JsonRpcTest, Invoke) {
 
+/// @test
+TEST_F(JsonRpcTest, InvokeMethod) {
     auto& dispatcher = server.GetDispatcher();
-    Math math;
-
     // if it is a member method, you must use this 3 parameter version, passing an instance of an object that implements it
-    dispatcher.AddMethod("add", &Math::Add, math);
-    dispatcher.AddMethod("add_array", &Math::AddArray, math);
+    dispatcher.AddMethod("add", &MethodsMock::Add, GlobalMock);
+    dispatcher.AddMethod("add_array", &MethodsMock::AddArray, GlobalMock);
 
-    // if it is just a regular function (non-member or static), you can you the 2 parameter AddMethod
-    dispatcher.AddMethod("concat", &Concat);
-    dispatcher.AddMethod("from_binary", &FromBinary);
-    dispatcher.AddMethod("to_struct", &ToStruct);
-    dispatcher.AddMethod("print_notification", &PrintNotification);
-
-    dispatcher.GetMethod("add").SetHelpText("Add two integers").AddSignature(
-            Json::Type::NUMBER, Json::Type::NUMBER, Json::Type::NUMBER);
-
-    std::string outputFormatedData;
+    expectedResponse = "{\"id\": 0, \"jsonrpc\": \"2.0\", \"result\": 5}";
+    EXPECT_CALL(GlobalMock, Add(3, 2)).WillOnce(Return(5));
+    response = server.HandleRequest(addRequest.c_str());
     printf("request %s\n", addRequest.c_str());
-    outputFormatedData = server.HandleRequest(addRequest.c_str());
-    printf("response: %s\n\n", outputFormatedData.c_str());
+    printf("response: %s\n\n", response.c_str());
 
-    printf("request %s\n", concatRequest.c_str());
-    outputFormatedData = server.HandleRequest(concatRequest.c_str());
-    printf("response: %s\n\n", outputFormatedData.c_str());
-
+    EXPECT_CALL(GlobalMock, AddArray(Json::array {1111, 2222})).WillOnce(Return(3333));
+    response = server.HandleRequest(addArrayRequest.c_str());
     printf("request %s\n", addArrayRequest.c_str());
-    outputFormatedData = server.HandleRequest(addArrayRequest.c_str());
-    printf("response: %s\n\n", outputFormatedData.c_str());
+    printf("response: %s\n\n", response.c_str());
+}
 
-    printf("request %s\n", toStructRequest.c_str());
-    outputFormatedData = server.HandleRequest(toStructRequest.c_str());
-    printf("response: %s\n\n", outputFormatedData.c_str());
+/// @test
+TEST_F(JsonRpcTest, InvokeStatic) {
+    auto& dispatcher = server.GetDispatcher();
+    // if it is just a regular function (non-member or static), you can you the 2 parameter AddMethod
+    dispatcher.AddMethod("concat", &StaticConcat);
+    dispatcher.AddMethod("to_object", &StaticToObject);
 
-    printf("request %s\n", printNotificationRequest.c_str());
-    outputFormatedData = server.HandleRequest(printNotificationRequest.c_str());
-    printf("response size: %d\n\n", (int)outputFormatedData.size());
+    // Concat
+    EXPECT_CALL(GlobalMock, Concat("Hello, ", "World!")).WillOnce(Return("Hello, World!"));
+    response = server.HandleRequest(concatRequest.c_str());
+    EXPECT_EQ(methods.Concat("Hello, ", "World!"), "Hello, World!");
+    expectedResponse = "{\"id\": 1, \"jsonrpc\": \"2.0\", \"result\": \"Hello, World!\"}";
+    EXPECT_EQ(response, expectedResponse);
+    printf("request %s\n", concatRequest.c_str());
+    printf("response: %s\n\n", response.c_str());
+
+
+    // To Object
+    std::string err;
+    std::string a = "[12,\"foobar\",[12,\"foobar\"]]";
+    auto array = Json::parse(a, err).array_items();
+    EXPECT_TRUE(err.empty());
+
+    std::string r = "{\"0\": 12, \"1\": \"foobar\", \"2\": [12, \"foobar\"]}";
+    auto object =  Json::parse(r, err).object_items();
+    EXPECT_TRUE(err.empty());
+
+    EXPECT_CALL(GlobalMock, ToObject(array)).WillOnce(Return(object));
+    response = server.HandleRequest(toObjectRequest.c_str());
+    EXPECT_EQ(methods.ToObject(array), object);
+    expectedResponse = "{\"id\": 4, \"jsonrpc\": \"2.0\", \"result\": " + r + "}";
+    EXPECT_EQ(response, expectedResponse);
+    printf("request %s\n", toObjectRequest.c_str());
+    printf("response: %s\n\n", response.c_str());
 }
 
